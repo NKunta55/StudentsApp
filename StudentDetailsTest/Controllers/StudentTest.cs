b3f96757b4987eaa9021f939940e61b559f38552
NarenderKunta <kuntanarender55@gmail.com>
2023-04-25 10:32:28 +0100

    Test

---
+++ StudentDetailsTest/Controllers/StudentTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentsCourseDetails.Classes;
+using StudentsCourseDetails.Controllers;
+using StudentsCourseDetails.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsCourseDetails.Controllers.Tests
+{
+   
+    public class StudentTest
+    {
+        [Test]
+        public void DetailsTest()
+        {
+            var controller = new StudentController();
+            var result = controller.Details(1) as PartialViewResult;
+            var student = (StudentModel)result.ViewData.Model;
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual("Narender", student.StudentName);
+        }
+        [Test]
+        public void UpdateStudentTest()
+        {
+            var student = new Student();
+        }
+    }
+}
---
+++ StudentDetailsTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using StudentsCourseDetails.Controllers;
+using StudentsCourseDetails.Models;
+
+namespace StudentDetailsTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+                       
+        }
+    }
+}
